cf001103c0a22897ec043dc2d5b824829ecec7de
Brian Dsouza <141020044+Briandsouza09@users.noreply.github.com>
2025-04-07 22:11:30 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-console.log('Environment Variables:', process.env);
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
